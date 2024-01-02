b8d067ccb9b9127d1cad2b14c582a8486b76a17e
BhargavRMsp <bhargav.r@msp-group.co.uk>
2024-01-02 17:51:35 +0530

    add rate-limiter

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { signIn, signUp } from "../controllers/authentication.js";
+import { rateLimitMiddleware } from "../middlewares/rateLimiter.js";
 
 const router = express.Router();
 
-router.route("/auth/sign-up").post(signUp);
-router.route("/auth/sign-in").post(signIn);
+router.route("/auth/sign-up").post(rateLimitMiddleware, signUp);
+router.route("/auth/sign-in").post(rateLimitMiddleware, signIn);
 
 export default router;
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,24 @@
 import express from "express";
 import { deleteUser, getAllUsers, updateUser } from "../controllers/user.js";
 import { isAuthenticated, isOwner } from "../middlewares/index.js";
+import { rateLimitMiddleware } from "../middlewares/rateLimiter.js";
 
 const router = express.Router();
 
-router.route("/users").get(isAuthenticated, getAllUsers);
-router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
-router.patch("/users/:id", isAuthenticated, isOwner, updateUser);
+router.route("/users").get(rateLimitMiddleware, isAuthenticated, getAllUsers);
+router.delete(
+  "/users/:id",
+  rateLimitMiddleware,
+  isAuthenticated,
+  isOwner,
+  deleteUser
+);
+router.patch(
+  "/users/:id",
+  rateLimitMiddleware,
+  isAuthenticated,
+  isOwner,
+  updateUser
+);
 
 export default router;
