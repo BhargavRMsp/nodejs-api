48dd27ddea9b8249270676fd588509cbad50e541
BhargavRMsp <bhargav.r@msp-group.co.uk>
2024-01-02 12:57:18 +0530

    Change URL

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 import app from "./app.js";
 import connectDatabase from "./config/database.js";
+import os from "os";
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Uncaught Exception`);
-    process.exit(1);
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
 });
 
 connectDatabase();
 
-
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+  console.log(
+    `Server is working on https://${process.env.DOMAIN}:${process.env.PORT}`
+  );
 });
 
 // Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
 
-    server.close(() => {
-        process.exit(1);
-    });
+  server.close(() => {
+    process.exit(1);
+  });
 });
