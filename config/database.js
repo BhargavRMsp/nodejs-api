e4f2f7aef7ceee4e1e6b9d27cc9fdd7c7dafefa1
BhargavRMsp <bhargav.r@msp-group.co.uk>
2024-01-03 10:23:50 +0530

    Remove deprecated mongoDB option

--- config/database.js
+++ config/database.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then((data) => {
-        console.log(`Mongodb connected with server: ${data.connection.host}`);
-    })
-}
+  mongoose.connect(process.env.DB_URL).then((data) => {
+    console.log(`Mongodb connected with server: ${data.connection.host}`);
+  });
+};
 
 export default connectDatabase;
